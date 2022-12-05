52409c4704a787deb3f3028ae2d412c65abf39ee
Varunsai-M <96068461+Varunsai-M@users.noreply.github.com>
2022-12-05 10:40:07 +0530

    Update and rename 1a.java to One_a.java

--- One_a.java
+++ One_a.java
@@ -19,7 +19,7 @@ double get_price(){
 return price;
 }
 }
-class 1a{
+class One_a{
 public static void main(String args[]){
 retail_item r1 = new retail_item("Kitkat", 10, 20.00);
 System.out.println("Description:"+r1.get_desc());
